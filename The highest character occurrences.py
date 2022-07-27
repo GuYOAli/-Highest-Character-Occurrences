9ef699237b017959666296842b72132be1a7c05e
Guled Ali <99419181+GuYOAli@users.noreply.github.com>
2022-07-26 18:13:39 -0700

    Update The highest character occurrences.py

--- The highest character occurrences.py
+++ The highest character occurrences.py
@@ -1,15 +1,23 @@
-# e is the first char appeared within this string
+from pprint import pprint
+
 sentence = "Hello, My_name is Guled Ali I'm junior_software_developer lol".lower()
 
-char_counter = {}
 
-for char in sentence:
-    if char in char_counter:
-        char_counter[char] += 1
-    else:
-        char_counter[char] = 1
+def char_frq(message):
+
+    char_counter = {}
+
+    for char in message:
+        if char in char_counter:
+            char_counter[char] += 1
+        else:
+            char_counter[char] = 1
+
+    return sorted(char_counter.items(), key=lambda kv: kv[1], reverse=True)
 
 
-sorted_char = sorted(char_counter.items(), key=lambda kv: kv[1], reverse=True)
+if __name__ == "__main__":
+    result = char_frq(sentence)
 
-print(sorted_char[0])
+pprint(result[0])  # e is the first char appeared within this string
+
